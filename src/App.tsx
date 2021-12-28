bf04fb19772e55858c8c393e5fa0c4f04e4795f4
Doris Gjata <dorisgjata@gmail.com>
2021-12-28 17:02:44 -0500

    feat: added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { Grid } from '@mui/material';
+import { Grid, ThemeProvider } from '@mui/material';
 import LettersGrid from './features/LettersGrid';
 import TwoLetterWords from './features/TwoLetterWords';
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f7da21'
+    },
+    secondary: {
+      main: '#e6e6e6',
+      dark: '#000'
+    }
+  }
+})
 function App() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={10} md={5}>
-        <LettersGrid />
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={2} maxWidth={1600}>
+        <Grid item xs={10} md={5}>
+          <LettersGrid />
+        </Grid>
+        <Grid item xs={10} md={5}>
+          <TwoLetterWords />
+        </Grid>
       </Grid>
-      <Grid item xs={10} md={5}>
-        <TwoLetterWords />
-      </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
 
