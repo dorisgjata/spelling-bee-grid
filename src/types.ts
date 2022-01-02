a27272f58a88145ebdde582c74ef43790ee165ff
Doris Gjata <dorisgjata@gmail.com>
2022-01-02 00:42:04 -0500

    feat: adding types

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface GridType {
+    letters: Letter[],
+    rowHeader: Letter[],
+    rows: Rows[]
+    twoLetterWords?: TwoLetterWords[]
+}
+export interface Rows {
+    letter: Letter,
+    repeats: Repeat[],
+}
+
+export interface TwoLetterWords {
+    letter: Letter,
+    repeat: Repeat,
+}
+export type Letter = string;
+export type Repeat = number;
