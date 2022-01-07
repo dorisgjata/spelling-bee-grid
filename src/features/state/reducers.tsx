4e942071d70dadbee53c80bee88e95c0006cab76
Doris Gjata <dorisgjata@gmail.com>
2022-01-06 22:06:26 -0500

    feat: adding proper reducers and types

--- src/features/state/reducers.tsx
+++ src/features/state/reducers.tsx
@@ -1,56 +1,81 @@
 import { Letter, Rows } from "../../types";
 
-const initialState = { words: [], repeats: [] };
+export type AppState = {
+    words: WordsType,
+    rows: RowsType
+};
+export type WordsType = string[];
+export type RowsType = {
+    letter: Letter,
+    repeats: number[]
+}[];
 
-interface State {
-    words: string[],
-    rows: {
-        letter: Letter,
-        repeats: number[]
-    }[],
-}
-interface WordsAction {
-    type: 'add' | 'delete',
-    payload: {
+export enum Types {
+    Add = 'add',
+    Delete = 'delete',
+    // Decrement = 'decrement',
+    // Increment = 'increment',
+};
+
+type ActionMap<M extends { [index: string]: any }> = {
+    [Key in keyof M]: M[Key] extends undefined
+    ? {
+        type: Key;
+    }
+    : {
+        type: Key;
+        payload: M[Key];
+    }
+};
+
+export type WordsPayload = {
+    [Types.Add]: {
+        word: string
+    },
+    [Types.Delete]: {
         word: string
     }
 }
 
-interface RowsAction {
-    type: 'increment' | 'decrement',
-    payload: {
-        word: string
-    }
-}
-export function wordsReducer(state: State, action: WordsAction) {
+// export type RowsPayload = {
+//     [Types.Increment]: {
+//         word: string,
+//     },
+//     [Types.Decrement]: {
+//         word: string,
+//     }
+// }
+export type WordsAction = ActionMap<WordsPayload>[keyof ActionMap<WordsPayload>];
+
+export function wordsReducer(state: AppState, action: WordsAction) {
     switch (action.type) {
-        case 'add':
-            return { ...state, words: [...state.words, action.payload.word] };
-        case 'delete':
+        case Types.Add:
+            return { words: [...state.words, action.payload.word], rows: handleRows(state.rows, action) };
+        case Types.Delete:
             const remaining = state.words.filter((word: string) => word !== action.payload.word);
-            return { ...state, words: remaining };
+            return { words: remaining, rows: handleRows(state.rows, action) };
         default:
-            state;
-
+            return state;
     }
 }
-export function reducer(state: State, action: RowsAction) {
-    switch (action.type) {
-        case 'decrement':
-            return { ...state, rows: handleRows(state, action) };
-        case 'increment':
-            return { ...state, rows: handleRows(state, action) };
-        default:
-            state;
+// export type RowsAction = ActionMap<RowsPayload>[keyof ActionMap<RowsPayload>];
 
-    }
-}
+// export function rowsReducer(state: RowsType, action: WordsAction | RowsAction) {
+//     switch (action.type) {
+//         case Types.Decrement:
+//             return handleRows(state, action);
+//         case Types.Increment:
+//             return handleRows(state, action);
+//         default:
+//             return state;
+//     }
+// }
 
-function handleRows(state: State, action: RowsAction) {
+function handleRows(state: RowsType, action: WordsAction) {
     const letter = action.payload.word.charAt(0).toUpperCase();
     const frequency = action.payload.word.length;
 
-    return state.rows.map((row: {
+    return state.map((row: {
         letter: Letter,
         repeats: number[]
     }) => {
@@ -58,17 +83,24 @@ function handleRows(state: State, action: RowsAction) {
             const updatedRow = {
                 ...row,
                 repeats: row.repeats.map((freq: number) => {
-                    if (freq === frequency && action.type === 'decrement') {
+                    if (freq === frequency && action.type === Types.Add) {
                         return freq - 1;
                     };
-                    if (freq === frequency && action.type === 'increment') {
+                    if (freq === frequency && action.type === Types.Delete) {
                         return freq + 1;
                     };
                     return freq;
                 })
             };
+            console.log(updatedRow);
             return updatedRow;
         }
         return row;
     });
 }
+
+
+// export const mainReducer = (state: AppState, action: WordsAction | RowsAction) => ({
+//     words: wordsReducer(state.words, action),
+//     rows: rowsReducer(state.rows, action),
+// });
